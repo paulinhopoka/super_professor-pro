b25cb095bc18428ff2956cec60493c6fb4ddae06
paulinhopoka <paulinhoandroidnf@gmail.com>
2026-03-23 17:50:00 -0300

    Update firebase.js
    edição manual para funcionar o login

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc, onSnapshot } from 'firebase/firestore';
-import firebaseConfig from './firebase-applet-config.json';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getFirestore, doc, getDoc, setDoc, onSnapshot } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+// Busca a configuração do arquivo JSON que você já tem no GitHub
+const response = await fetch('./firebase-applet-config.json');
+const firebaseConfig = await response.json();
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
