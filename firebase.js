291c325bb59e8df99b6170d7efac7a5341680e1a
paulinhopoka <paulinhoandroidnf@gmail.com>
2026-03-23 18:31:26 -0300

    Update firebase.js
    tentandooo

--- firebase.js
+++ firebase.js
@@ -1,14 +1,19 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
-import { getFirestore, doc, getDoc, setDoc, onSnapshot } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+import { getFirestore, doc, getDoc, setDoc, onSnapshot, collection, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
-// Busca a configuração do arquivo JSON que você já tem no GitHub
-const response = await fetch('./firebase-applet-config.json');
-const firebaseConfig = await response.json();
+// Busca a configuração do arquivo JSON que já está no seu GitHub
+const configResponse = await fetch('./firebase-applet-config.json');
+const firebaseConfig = await configResponse.json();
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const provider = new GoogleAuthProvider();
 
-export { signInWithPopup, signOut, onAuthStateChanged, doc, getDoc, setDoc, onSnapshot, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export { 
+  signInWithPopup, signOut, onAuthStateChanged, 
+  doc, getDoc, setDoc, onSnapshot, 
+  createUserWithEmailAndPassword, signInWithEmailAndPassword,
+  collection, getDocs, query, where
+};
