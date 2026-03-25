2fb3770fea01eaacb50b705fdfe188ab594f96e3
paulinhopoka <paulinhoandroidnf@gmail.com>
2026-03-25 20:06:13 -0300

    Add files via upload
    correção mapa de sala e aba detalhes de turma

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
-import '../style.css';
-import '../detalhes-turma.css';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
