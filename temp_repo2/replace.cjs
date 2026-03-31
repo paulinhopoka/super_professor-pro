fee617f5bd3b288ad1f2e8ecabbaa6aee92d1fd2
paulinhopoka <paulinhoandroidnf@gmail.com>
2026-03-31 13:49:32 -0300

    Delete temp_repo2 directory

--- temp_repo2/replace.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('script.js', 'utf8');
-content = content.replace(/\balert\(/g, 'customAlert(');
-fs.writeFileSync('script.js', content);
-console.log('Done');
